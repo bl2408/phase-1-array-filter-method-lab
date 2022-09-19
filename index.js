771eb2e1bf7e9132b9e93907d5e12d65e2441396
Brian Lambert <brianlambert2408@gmail.com>
2022-09-20 08:08:38 +1000

    Completed tasks

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(driverList, driver){
+    const list = driverList.filter(el=> el.toLowerCase() === driver.toLowerCase());
+    return list;
+}
+
+function fuzzyMatch(driverList, driver){
+    const list = driverList.filter(el=>el[0].toLowerCase() === driver[0].toLowerCase());
+    return list;
+}
+
+function matchName(driverList, driver){
+    const list = driverList.filter(el=>el.name.toLowerCase() === driver.toLowerCase());
+    return list;
+}
